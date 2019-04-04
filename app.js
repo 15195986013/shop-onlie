1f3b69e68ca549366be4b80da26627b1b81eb29d
lijiancheng <1833717745@qq.com>
2019-04-04 15:40:20 +0800

    // 修改成本地头像

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ App({
     globalData: {
         userInfo: {
             nickname: '点击登录',
-            avatar: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+            avatar: '/static/images/default.png'
         },
         token: '',
     }
