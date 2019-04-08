f955f75e1c60791d80480e8bec1a9cff55bed493
lijiancheng <1833717745@qq.com>
2019-04-08 13:31:31 +0800

    // 订单支付成功后的状态更新

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -3,72 +3,73 @@ var util = require('../../utils/util.js');
 var api = require('../../config/api.js');
 
 Page({
-  data: {
-    orderId: '6f2eb93eff4f49f4a246da68d9d490a9',
-    actualPrice: 0.10
-  },
-  onLoad: function(options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    this.setData({
-      orderId: options.orderId,
-      actualPrice: options.actualPrice
-    })
-  },
-  onReady: function() {
-
-  },
-  onShow: function() {
-    // 页面显示
-
-  },
-  onHide: function() {
-    // 页面隐藏
-
-  },
-  onUnload: function() {
-    // 页面关闭
-
-  },
-  //向服务请求支付参数
-  requestPayParam() {
-    let that = this;
-    util.request(api.PayPrepayId, {
-      orderId: that.data.orderId,
-      payType: 1
-    }).then(function(res) {
-      if (res.errno === 0) {
-        let payParam = res.data;
-        wx.requestPayment({
-          'timeStamp': payParam.timeStamp,
-          'nonceStr': payParam.timeStamp,
-          'package': payParam.nonceStr,
-          'signType': payParam.signType,
-          'paySign': payParam.paySign,
-          'success': function(res) {
-            util.request(api.OrderUpdate,{
-              orderId: that.data.orderId,
-              payType: 1
-            }).then(function(res) {
-              if (res.errno === 0) {
-                wx.redirectTo({
-                  url: '/pages/payResult/payResult?status=true',
-                })
-              }
-            });
-            
-          },
-          'fail': function(res) {
-            wx.redirectTo({
-              url: '/pages/payResult/payResult?status=false',
-            })
-          }
+    data: {
+        orderId: '6f2eb93eff4f49f4a246da68d9d490a9',
+        actualPrice: 0.10
+    },
+    onLoad: function (options) {
+        // 页面初始化 options为页面跳转所带来的参数
+        this.setData({
+            orderId: options.orderId,
+            actualPrice: options.actualPrice
         })
-      } else {
-        util.showErrorToast(res.errmsg)
-      }
-    });
-  },
-  startPay() {
-    this.requestPayParam();
-  }
-})
+    },
+    onReady: function () {
+
+    },
+    onShow: function () {
+        // 页面显示
+
+    },
+    onHide: function () {
+        // 页面隐藏
+
+    },
+    onUnload: function () {
+        // 页面关闭
+
+    },
+    //向服务请求支付参数
+    requestPayParam() {
+        let that = this;
+        util.request(api.PayPrepayId, {
+            orderId: that.data.orderId,
+            payType: 1
+        }).then(function (res) {
+            if (res.errno === 0) {
+                let payParam = res.data;
+                wx.requestPayment({
+                    'timeStamp': payParam.timeStamp,
+                    'nonceStr': payParam.timeStamp,
+                    'package': payParam.nonceStr,
+                    'signType': payParam.signType,
+                    'paySign': payParam.paySign,
+                    'success': function (res) {
+                        if (res.errno === 0) {
+                            util.request(api.OrderUpdate, {
+                                orderId: that.data.orderId,
+                                payType: 1
+                            }).then(function (res) {
+                                if (res.errno === 0) {
+                                    wx.redirectTo({
+                                        url: '/pages/payResult/payResult?status=true',
+                                    })
+                                }
+                            });
+                        }
+                    },
+                    'fail': function (res) {
+                        wx.redirectTo({
+                            url: '/pages/payResult/payResult?status=false',
+                        })
+                    }
+                })
+            } else {
+                util.showErrorToast(res.errmsg)
+            }
+        });
+    },
+    startPay() {
+        this.requestPayParam();
+    }
+});
--- pages/ucenter/order/order.js
+++ pages/ucenter/order/order.js
@@ -2,15 +2,15 @@ var util = require('../../../utils/util.js');
 var api = require('../../../config/api.js');
 
 Page({
-  data:{
+  data: {
     orderList: []
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
 
     this.getOrderList();
   },
-  getOrderList(){
+  getOrderList() {
     let that = this;
     util.request(api.OrderList).then(function (res) {
       if (res.errno === 0) {
@@ -21,23 +21,23 @@ Page({
       }
     });
   },
-  
-  payOrder(event){
-    let that= this;
+
+  payOrder(event) {
+    let that = this;
     wx.redirectTo({
-      url: '/pages/pay/pay?orderId=' + event.currentTarget.dataset.orderId+'&actualPrice=' + event.currentTarget.dataset.orderPrice,
-    })
+      url: '/pages/pay/pay?orderId=' + event.currentTarget.dataset.orderId + '&actualPrice=' + event.currentTarget.dataset.orderPrice,
+    });
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
-})
+});
